83a5631cda0cff8930e38d26d6d55def67be01e3
Kenn Stack <kibadikennedy@gmail.com>
2021-10-14 15:04:08 +0800

    built the auth form

--- components/Auth.js
+++ components/Auth.js
@@ -32,11 +32,55 @@ export default function Auth() {
             Accéder maintenant
           </h1>
           <div className="grid grid-cols-1 divide-y divide-gray-400">
+            <form className="w-full max-w-sm mb-5">
+              <div className="md:flex md:items-center mb-2 mt-5">
+                <div className="w-full">
+                  <input
+                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 text-sm leading-tight focus:outline-none focus:bg-white focus:border-back-color-500"
+                    id="inline-full-name"
+                    type="text"
+                    value=""
+                    placeholder="Nom d'utilisateur"
+                  />
+                </div>
+              </div>
+              <div className="md:flex md:items-center mb-6">
+                <div className="w-full">
+                  <input
+                    class="bg-gray-200 appearance-none border-2 my-auto border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-back-color-500"
+                    id="inline-password"
+                    type="password"
+                    placeholder="******************"
+                  />
+                </div>
+              </div>
+              <div className="md:flex md:items-center mb-6">
+                <label className="block md:flex flex-row text-gray-500 font-bold cursor-pointer">
+                  <input
+                    className="mr-2 leading-tight form-checkbox h-4 w-4 rounded-full my-auto"
+                    type="checkbox"
+                  />
+                  <span className="text-sm my-auto">
+                    Send me your newsletter!
+                  </span>
+                </label>
+              </div>
+              <div className="md:flex">
+                <div className="">
+                  <button
+                    className="shadow bg-light-pink-500 hover:bg-light-pink-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                    type="button"
+                  >
+                    Se Connecter
+                  </button>
+                </div>
+              </div>
+            </form>
             <div className="flex flex-col mb-2">
               {/* Github */}
               <button
                 //   onClick={handleLogin}
-                className="bg-white outline-none shadow-sm hover:shadow-lg mx-auto px-10 font-semibold py-2 my-3 rounded-lg"
+                className="bg-white outline-none shadow-sm hover:shadow-lg mx-auto px-10 font-semibold py-2 my-3 mt-4 rounded-lg"
                 style={{ background: "#211F1F" }}
               >
                 <div className="flex flex-row text-white">
@@ -76,62 +120,6 @@ export default function Auth() {
                 </div>
               </button>
             </div>
-            <form className="w-full max-w-sm">
-              <div className="md:flex md:items-center mb-2 mt-10">
-                <div className="md:w-1/3">
-                  <label
-                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                    for="inline-full-name"
-                  >
-                    Full Name
-                  </label>
-                </div>
-                <div className="md:w-2/3">
-                  <input
-                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                    id="inline-full-name"
-                    type="text"
-                    value="Jane Doe"
-                  />
-                </div>
-              </div>
-              <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3">
-                  <label
-                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                    for="inline-password"
-                  >
-                    Password
-                  </label>
-                </div>
-                <div className="md:w-2/3">
-                  <input
-                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                    id="inline-password"
-                    type="password"
-                    placeholder="******************"
-                  />
-                </div>
-              </div>
-              <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3"></div>
-                <label className="md:w-2/3 block text-gray-500 font-bold">
-                  <input className="mr-2 leading-tight" type="checkbox" />
-                  <span className="text-sm">Send me your newsletter!</span>
-                </label>
-              </div>
-              <div className="md:flex md:items-center">
-                <div className="md:w-1/3"></div>
-                <div className="md:w-2/3">
-                  <button
-                    className="shadow bg-light-pink-500 hover:bg-light-pink-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                    type="button"
-                  >
-                    Se Connecter
-                  </button>
-                </div>
-              </div>
-            </form>
           </div>
         </div>
       </div>
