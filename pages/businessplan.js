2804114744d6337c49f1d9406adaa2f6dc065b05
Kenn Stack <kibadikennedy@gmail.com>
2021-10-26 20:42:25 +0800

    built Business plan filter

--- pages/businessplan.js
+++ pages/businessplan.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Layout from "../components/Layout";
 import Hero from "../components/BusinessPlan/Hero";
 import BusinessPlanALaUneBanner from "../components/BusinessPlan/BusinessPlanALaUne/BpALaUneBanner";
+import BusinessPlansList from "../components/BusinessPlan/BusinessPlansList";
 
 export default function BusinessPlan() {
   return (
@@ -14,6 +15,7 @@ export default function BusinessPlan() {
       <Layout>
         <Hero />
         <BusinessPlanALaUneBanner />
+        <BusinessPlansList />
       </Layout>
     </div>
   );
