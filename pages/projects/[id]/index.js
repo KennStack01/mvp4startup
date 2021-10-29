8c442f05e48efb79a12a571139d7700ee2fb8e56
Kenn Stack <kibadikennedy@gmail.com>
2021-10-29 21:37:01 +0800

    added embed video

--- pages/projects/[id]/index.js
+++ pages/projects/[id]/index.js
@@ -4,6 +4,12 @@ import Head from "next/head";
 const ProjectComponent = ({ project }) => {
   console.log(project);
 
+  const videoID = project.introVideo.replace(
+    "https://www.youtube.com/watch?v=",
+    ""
+  );
+  const embedURL = `https://www.youtube.com/embed/${videoID}`;
+
   return (
     <div>
       <Head>
@@ -12,11 +18,25 @@ const ProjectComponent = ({ project }) => {
       </Head>
 
       <Layout>
-        <div className="text-gray-800 font-semibold text-center mx-auto text-xl md:text-2xl mt-8">
-          Page Project ID: {project.id} <br />
-          Page Project Slug: {project.slug}
+        <div className="flex flex-col">
+          <h1 className="text-xl font-semibold text-center text-gray-900">
+            {" "}
+            {project.title}{" "}
+          </h1>
+          <div className="mx-2 md:mx-72 my-4 md:my-8">
+            <iframe
+              width="100%"
+              height="100%"
+              src={embedURL}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title={project.title}
+              // className="rounded-lg mx-3 my-4 md:my-8 w-full md:w-5/6 h-80"
+              className="rounded-lg mx-auto h-96"
+            />
+          </div>
         </div>
-        <img src={project.imageSrc} alt="Project Pic" />
       </Layout>
     </div>
   );
