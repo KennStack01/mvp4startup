f07d248226c0f7445dcc9d592fe1ea7afa681789
Kenn Stack <kibadikennedy@gmail.com>
2021-11-06 21:47:53 +0800

    working on the blog

--- components/Home/Formation/FormationsList.js
+++ components/Home/Formation/FormationsList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Link from "next/link";
 import Formation from "./Formation";
 import { BsArrowRightShort } from "react-icons/bs";
