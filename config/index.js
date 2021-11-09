1d1769a47807e878e5625352f61b2522c69ac281
Kenn Stack <kibadikennedy@gmail.com>
2021-11-09 16:47:24 +0800

    solving next-auth issue AGAIN haha

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "https://mvp4startup-api.herokuapp.com"
-  : "http://localhost:3000";
+  : "https://mvp4startup-api.herokuapp.com";
