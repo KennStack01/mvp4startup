8c531ab09284a660959734a8a8791e8bccd66fec
Kenn Stack <kibadikennedy@gmail.com>
2021-11-09 16:28:45 +0800

    solving next-auth issue

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const withReactSvg = require("next-react-svg");
 const path = require("path");
+const dev = process.env.NODE_ENV !== "production";
+
+module.exports = dev
+  ? "http://localhost:3000"
+  : "https://mvp4startup-api.herokuapp.com";
 
 module.exports = withReactSvg({
   include: path.resolve(__dirname, "public/svg"),
