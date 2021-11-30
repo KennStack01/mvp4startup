6e78e88d98c82fd04d0e432b505e2a61dedeb62b
Kenn Stack <kibadikennedy@gmail.com>
2021-11-30 14:08:25 +0800

    handling api error

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -27,18 +27,41 @@ export default function Index({ projects, formations }) {
 
 export async function getStaticProps() {
   // const projects = await fetch("http://localhost:3002/projects").then((res) =>
-  const projects = await fetch(`${server}/api/v1/projects`).then((res) =>
-    res.json()
-  );
 
-  const formations = await fetch(`${server}/api/v1/formations`).then((res) =>
-    res.json()
-  );
+  try {
+    const res = await fetch(`${server}/api/v1/projects`);
+    const projects = await res.json();
+    const formations = await fetch(`${server}/api/v1/formations`).then((res) =>
+      res.json()
+    );
+    return {
+      props: {
+        projects,
+        formations,
+      },
+    };
+  } catch (error) {
+    console.log(error.message);
+  }
 
   return {
     props: {
-      projects,
-      formations,
+      projects: [],
+      formations: [],
     },
   };
+  // const projects = await fetch(`${server}/api/v1/projects`).then((res) =>
+  //   res.json()
+  // );
+
+  // const formations = await fetch(`${server}/api/v1/formations`).then((res) =>
+  //   res.json()
+  // );
+
+  // return {
+  //   props: {
+  //     projects,
+  //     formations,
+  //   },
+  // };
 }
